264532624a5ede0d5cc6cc63f7b1ccb865989883
ghorbani <aali361@gmail.com>
2020-07-10 14:03:54 +0430

    add Bot & Channel model

--- bot/navid/admin.py
+++ bot/navid/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Channel, Bot
+
+@admin.register(Channel)
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'username')
+    search_fields = ['name', 'username']
+
+
+@admin.register(Bot)
+class BotAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'token')
+    search_fields = ['name']
--- bot/navid/models.py
+++ bot/navid/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Channel(BaseModel):
+    name = models.CharField(blank=True, max_length=100, null=True)
+    username = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Bot(BaseModel):
+    name = models.CharField(blank=True, max_length=100, null=True)
+    token = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
