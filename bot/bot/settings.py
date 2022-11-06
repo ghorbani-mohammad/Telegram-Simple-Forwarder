e7959c8b52a1880b93edec175a174d01f9be15bd
Mohammad <mohammad.ghorbani@hotmail.com>
2022-11-06 19:05:36 +0330

    minor change

--- bot/bot/settings.py
+++ bot/bot/settings.py
@@ -1,6 +1,5 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "n76e8os4x0-#993gv+(#v^^ufql_#_)&$67hi8es55g0*drik8"
 
