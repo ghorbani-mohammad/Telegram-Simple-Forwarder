8d9b26e89f09f85daadc95130c30e589f63a3b16
Mohammad <mohammad.ghorbani@hotmail.com>
2022-11-06 17:39:00 +0330

    minor change

--- bot/navid/admin.py
+++ bot/navid/admin.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 
 from .models import Channel, Broker
 
+
 @admin.register(Channel)
 class ChannelAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'username')
-    search_fields = ['name', 'username']
+    list_display = ("id", "name", "username")
+    search_fields = ["name", "username"]
 
 
 @admin.register(Broker)
 class BrokerAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
-    search_fields = ['name']
+    list_display = ("id", "name")
+    search_fields = ["name"]
