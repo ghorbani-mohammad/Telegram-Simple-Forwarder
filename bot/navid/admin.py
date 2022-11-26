416e1eed041654f04dba700694a584d889cc79ee
Mohammad <mohammad.ghorbani@hotmail.com>
2022-11-26 20:09:31 +0330

    minor change

--- bot/navid/admin.py
+++ bot/navid/admin.py
@@ -18,8 +18,5 @@ class ChannelAdmin(admin.ModelAdmin):
 
 @admin.register(Broker)
 class BrokerAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "name",
-    )
+    list_display = ("id", "name")
     search_fields = ("name",)
